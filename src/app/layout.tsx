a2f101761482def9c092fffe6b97761ec232fcd2
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-08-01 14:20:08 +0100

    feat: Add shadcn with some components

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "../lib/ui/utils";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cabinet Demo",
+  description: "A demo of Cabinet management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      suppressHydrationWarning
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange>
+          {/* <SupabaseProvider>
+            <UserProvider>{children}</UserProvider>
+          </SupabaseProvider> */}
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
